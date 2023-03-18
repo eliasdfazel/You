c5b2941121b55e2aecce414875d3af4e3986d63c
Elias Fazel <elias.fazel@gmail.com>
2023-03-18 10:05:44 -0700

    [Description]
    [ChangeLog]
    ~ Initializing Project
    
    [Build][ChangeLog]
    # Dependencies Updated

---
+++ You/src/main/java/co/geeksempire/frames/you/Dashboard/UI/Dashboard.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2023 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 3/18/23, 10:04 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.frames.you.Dashboard.UI
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Dashboard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
