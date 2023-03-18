1417342d0cfdb01549c11d8e3b773f288236c1a3
Elias Fazel <elias.fazel@gmail.com>
2023-03-18 10:50:44 -0700

    [Description]
    [ChangeLog]
    ~ Initializing Project
    
    [Build][ChangeLog]
    # Dependencies Updated

--- You/src/main/java/co/geeksempire/frames/you/Dashboard/Extensions/UserInterface.kt
+++ You/src/main/java/co/geeksempire/frames/you/Dashboard/Extensions/UserInterface.kt
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 3/18/23, 10:49 AM
+ * Last modified 3/18/23, 10:50 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -33,6 +33,10 @@ fun Dashboard.setupUserInterface() {
     if (systemSettings.floatingPermissionEnabled()) {
 
         dashboardLayoutBinding.floatingPermission.root.visibility = View.GONE
+
+        /*
+         * Call Firestore
+         */
 
     } else {
 
